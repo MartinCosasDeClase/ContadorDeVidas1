122195ba75268cb71160820d96ab9ef82d6fd432
MartinCosasDeClase <martincosasdeclase@gmail.com>
2024-09-26 13:45:10 +0200

    Commit

--- app/src/main/java/com/example/contadordevidas/FirstFragment.kt
+++ app/src/main/java/com/example/contadordevidas/FirstFragment.kt
@@ -13,6 +13,11 @@ import com.example.contadordevidas.databinding.FragmentFirstBinding
  */
 class FirstFragment : Fragment() {
 
+    private var vidaP1 = 20;
+    private var venenoP1 = 0;
+    private var vidaP2 = 20;
+    private var venenoP2 = 0;
+
     private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
@@ -29,16 +34,56 @@ class FirstFragment : Fragment() {
 
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
-    }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //BOTONE P1----------------------------------------------------------
+
+        binding.btnpvp1.setOnClickListener {
+            vidaP1++;
+            actualizaVidaP1()
+        }
+        binding.btnmvp1.setOnClickListener {
+            vidaP1--
+            actualizaVidaP1()
+        }
+        binding.btnPpP1.setOnClickListener {
+            venenoP1++
+            actualizaVidaP1()
+        }
+        binding.btnPmP1.setOnClickListener {
+            venenoP1--
+            actualizaVidaP1()
+        }
+        //BOTONES P2 ------------------------------------------------
+
+        binding.btnpvp2.setOnClickListener{
+            vidaP2++
+            actualizaVidaP2()
+        }
+        binding.btnmvp2.setOnClickListener{
+            vidaP2--
+            actualizaVidaP2()
+        }
+        binding.btnPpP2.setOnClickListener{
+            venenoP2++
+            actualizaVidaP2()
+        }
+        binding.btnPmP2.setOnClickListener{
+            venenoP2--
+            actualizaVidaP2()
+        }
+
+    }
+
+    private fun actualizaVidaP1() {
+        binding.textView.setText("" + vidaP1 + "/" + venenoP1)
+    }
+    private fun actualizaVidaP2(){
+        binding.textView2.setText(""+ vidaP2 + "/" + venenoP2)
     }
 }
